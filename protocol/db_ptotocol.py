f95d58cb8d896af792ec940c2b6bd017d1f295f5
HD-PC <251455882@qq.com>
2018-12-24 11:43:25 +0800

    电表协议代码规范化

--- protocol/db_ptotocol.py
+++ protocol/db_ptotocol.py
@@ -5,33 +5,14 @@ __title__ = ''
 __author__ = 'ZengXu'
 __mtime__ = '2018-10-9'
 """
-import binascii
 import datetime
-import json
-import logging
-import os
-import random
-import re
-import struct
-import sys
-import threading
-import time
-from abc import ABCMeta, abstractmethod
-from collections import defaultdict
-
 import APIs.common_APIs as common_APIs
 import connections.my_socket as my_socket
-from APIs.common_APIs import crc, crc16, protocol_data_printB
-from APIs.security import AES_CBC_decrypt, AES_CBC_encrypt
-from protocol.protocol_process import communication_base
-import struct
-import json
 from protocol.protocol_process import communication_base
 try:
     import queue as Queue
 except:
     import Queue
-
 
 class MessageType(object):
     '''处理电表协议的信息'''
@@ -78,7 +59,6 @@ class MessageType(object):
         return r
     def __processCmd(self):
         return self.cmd^0x80
-
     def set_addr(self,newaddr:str):
         realaddr = newaddr
         if len(newaddr) > self.addrlen*2:
@@ -89,7 +69,6 @@ class MessageType(object):
         for i in range(0, len(realaddr), 2):
             addr.append(int(realaddr[i:i + 2], 16))
         self.addr = bytes(addr)
-
     def set_datafield(self,dataDict:dict):
         datalen = 3
         if self.data_field_len >=4:
@@ -112,7 +91,6 @@ class MessageType(object):
             self.data_field_len = datalen  + self.data_field_len
             return None
         return "Field len is {}".format(self.data_field_len)
-
     def buildRspMessage(self):
         msg = bytearray()
         msg.extend(self.prefix_head)
@@ -127,9 +105,7 @@ class MessageType(object):
         self.cs = self.__calcs()
         msg.extend(self.cs.to_bytes(1,'big'))
         msg.extend(self.tail)
-        #msg2 = self.prefix_head + self.frame_head+ self.addr+self.frame_head+self.cmd+self.data_field_len+self.data+self.cs+self.tail
         return bytes(msg)
-
     def getBinStr(self, data):
         msg = ''
         if data:
@@ -148,47 +124,14 @@ class DB_Protocol(communication_base):
         self.encrypt_flag = encrypt_flag
         self.name = 'Device controler'
         self.connection = my_socket.MyClient(logger, addr, self_addr)
-        self.state = 'close'
         self.sim_obj = None
-
-        # status data:
-        # 4bytes
-        self.version = b'HDXM'
-        # 20bytes
-        self.device_id = b'\x31\x30\x30\x34\x32\x30\x31\x36\x35\x38\x46\x43\x44\x42\x44\x38\x33\x34\x31\x45'
-        # 4bytes
-        self.pkg_number = b'\x00\x00\x00\x01'
 
         #region 电表报文头相关变量
         self.prefixHead = b'\xFE\xFE\xFE\xFE'
         #endregion
 
-    def msg_build(self, data, ack=b'\x01'):
-        self.LOG.yinfo("send msg: " + self.convert_to_dictstr(data))
-        # need encrypt
-        if self.encrypt_flag:
-            data = AES_CBC_encrypt(self.sim_obj._encrypt_key, data)
-        if isinstance(data, type(b'')):
-            pass
-        else:
-            data = data.encode('utf-8')
-        if ack == b'\x00':
-            src_id = self.device_id
-            dst_id = b'\x30' * 20
-            self.add_pkg_number()
-        else:
-            src_id = self.device_id
-            dst_id = b'\x30' * 20
-
-        if isinstance(src_id, type(b'')):
-            pass
-        else:
-            src_id = src_id.encode('utf-8')
-        msg_head = self.version + src_id + dst_id + ack + self.pkg_number
-        msg_length = self.get_msg_length(data)
-        msg_crc16 = crc16(data)
-        msg = msg_head + msg_length + b'\x00\x00' + msg_crc16 + data
-        return msg
+    def msg_build(self, msg:MessageType):
+        return msg.buildRspMessage()
 
     def protocol_data_washer(self, data):
         data_list = []
@@ -227,20 +170,6 @@ class DB_Protocol(communication_base):
 
         return data_list, left_data
 
-    def add_pkg_number(self):
-        pkg_number = struct.unpack('>I', self.pkg_number)[0]
-        pkg_number += 1
-        self.pkg_number = struct.pack('>I', pkg_number)
-
-    def get_pkg_number(self, data):
-        return struct.unpack('>I', data)[0]
-
-    def set_pkg_number(self, data):
-        self.pkg_number = data
-
-    def get_msg_length(self, msg):
-        return struct.pack('>I', len(msg))
-
     def protocol_handler(self, msg):
         ack = False
         if msg[0:4] == self.prefixHead:
@@ -249,35 +178,10 @@ class DB_Protocol(communication_base):
             self.LOG.info("recv msg: " + common_APIs.protocol_data_printB(msg))
             rsp_msg:MessageType = self.sim_obj.protocol_handler(msg, ack)
             if rsp_msg:
-                #final_rsp_msg = self.msg_build(rsp_msg)
-                final_rsp_msg = rsp_msg.buildRspMessage()
+                final_rsp_msg =  self.msg_build(rsp_msg)#rsp_msg.buildRspMessage()
             else:
                 final_rsp_msg = 'No_need_send'
             return final_rsp_msg
-            '''if True or msg[4 + 20:4 + 20 + 20] == self.device_id or msg[4 + 20:4 + 20 + 20] == self.device_id.encode('utf-8'):
-                if msg[44:45] != b'\x00':
-                    # self.LOG.info("Get ack!")
-                    ack = True
-                self.set_pkg_number(msg[45:49])
-                data_length = struct.unpack('>I', msg[49:53])[0]
-                crc16 = struct.unpack('>H', msg[55:57])
-                # need decrypt
-                if self.encrypt_flag:
-                    data = json.loads(AES_CBC_decrypt(self.sim_obj._encrypt_key,
-                                                      msg[57:57 + data_length]).decode('utf-8'))
-                else:
-                    data = json.loads(msg[57:57 + data_length].decode('utf-8'))
-                self.LOG.info("recv msg: " + self.convert_to_dictstr(data))
-                rsp_msg = self.sim_obj.protocol_handler(data, ack)
-                if rsp_msg:
-                    final_rsp_msg = self.msg_build(rsp_msg)
-                else:
-                    final_rsp_msg = 'No_need_send'
-                return final_rsp_msg
-
-            else:
-                self.LOG.error('Unknow msg: %s' % (msg))
-                return "No_need_send"'''
         else:
             self.LOG.warn('Unknow msg: %s!' % (msg))
             return "No_need_send"
@@ -308,14 +212,13 @@ class DB_Protocol(communication_base):
 
     def recv_data(self):
         return self.connection.recv_once()
-import time
+
 if __name__ == '__main__':
+    import time
     start = datetime.datetime.now()
     #data = b'\xfe\xfe\xfe\xfe\x68\xaa\xaa\xaa\xaa\xaa\xaa\x68\x13\x00\xdf\x16'
     data = b'\xfe\xfe\xfe\xfe\x68\xaa\xaa\xaa\xaa\xaa\xaa\x68\x13\x05\x45\x67\x89\xab\xc3\x87\x16'
     m = MessageType(data)
-    #testbin = b'\x45\x67\x89\xab\xc3'
-    #newdata = m._MessageType__process33h("-")
     m.set_addr("598888881000")
     newdata = m.buildRspMessage()
     m.getBinStr(data)
